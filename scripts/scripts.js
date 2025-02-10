1a0878ad8291d5ef31645edd5b1e138264dd1f9a
Bibax68 <bibax68@gmail.com>
2025-02-10 14:05:12 +0100

    fetch et affichage des recettes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,17 +1,43 @@
 import { recipes } from '../json/recipes.js';
 
-// Exemple d'utilisation :
-async function displayRecipes() {
+
+function displayRecipes() {
+    const recipeCardArea = document.querySelector('.recipe_card_area');
+    recipeCardArea.innerHTML = ''; // Vide la zone avant d'ajouter les nouvelles recettes
+
     recipes.forEach(recipe => {
-        console.log(recipe);
-        
+        const recipeCard = `
+            <article class="recipe_card">
+                <img src="./pictures/recipes/${recipe.image}" alt="${recipe.name}" />
+                <span class="temps_recette">${recipe.time}min</span>
+                <h2>${recipe.name}</h2>
+                <p>${recipe.description}</p>
+                <ul>
+                    ${recipe.ingredients.map(ing => `
+                        <li>${ing.ingredient}${ing.quantity ? ' : ' + ing.quantity : ''}${ing.unit ? ' ' + ing.unit : ''}</li>
+                    `).join('')}
+                </ul>
+            </article>
+        `;
+        recipeCardArea.innerHTML += recipeCard;
     });
 }
 
-displayRecipes();
+// Initialisation au chargement de la page
+document.addEventListener('DOMContentLoaded', () => {
+    displayRecipes();
+});
+// Exemple d'utilisation :
+// async function displayRecipes() {
+//     recipes.forEach(recipe => {
+//         console.log(recipe);
+//     });
+// }
 
-async function init () {
-    const recipes = await displayRecipes();
-}
+// displayRecipes();
 
-init();
+// async function init () {
+//     const recipes = await displayRecipes();
+// }
+
+// init();
